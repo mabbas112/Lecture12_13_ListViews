392bbc1cd19f1034e7a7e1c6476acfc9476b2a8f
mabbas <mabbasctn69@gmail.com>
2021-11-19 19:55:30 +0500

    Button , textview add. But first display elements from arrayList to list view using arrayAdapter. Which convert element as view

--- app/src/main/java/com/example/lecture12_13_listviews/MainActivity.java
+++ app/src/main/java/com/example/lecture12_13_listviews/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.lecture12_13_listviews;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayList<String> friendList=new ArrayList<>();
+        friendList.add("Abbas");
+        friendList.add("ALi");
+        friendList.add("Haidar");
+        friendList.add("Fatima");
+        friendList.add("Sami");
+        ListView lv= findViewById(R.id.listView);
+        ArrayAdapter<String> arrayAdapter= new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,friendList);
+        lv.setAdapter(arrayAdapter);
+
     }
 }
